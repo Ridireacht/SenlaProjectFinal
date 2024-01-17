2148ded171daba115c802ad1cc37c631701ece5b
Ridireacht <ridiredire@gmail.com>
2024-01-17 20:59:52 +0300

    Add role for User

--- src/main/java/com/senla/project/entities/User.java
+++ src/main/java/com/senla/project/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,4 +24,7 @@ public class User {
   private String email;
   private String password;
 
+  @ManyToOne
+  @JoinColumn(name = "role_id")
+  private Role role;
 }
