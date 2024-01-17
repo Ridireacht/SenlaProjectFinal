15fb500347ce2192ee95c6eaba1749c60a056b82
Ridireacht <ridiredire@gmail.com>
2024-01-18 01:06:22 +0300

    Add Ad controller

---
+++ src/main/java/com/senla/project/controllers/AdController.java
@@ -0,0 +1,58 @@
+package com.senla.project.controllers;
+
+import com.senla.project.dto.AdRequest;
+import com.senla.project.dto.AdResponse;
+import com.senla.project.services.AdService;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ads")
+@AllArgsConstructor
+public class AdController {
+
+  private final AdService adService;
+
+
+  @GetMapping
+  public List<AdResponse> getAllAds() {
+    return adService.getAllAds();
+  }
+
+  @GetMapping("/my")
+  public List<AdResponse> getMyAds() {
+    return adService.getAdsByUserId();
+  }
+
+  @GetMapping("/{id}")
+  public AdResponse getAd(@PathVariable("id") Long id) {
+    return adService.getAdById();
+  }
+
+  @PostMapping
+  public AdResponse createAd(AdRequest adRequest) {
+    return adService.createAd(adRequest);
+  }
+
+  @PutMapping("/{id}")
+  public AdResponse updateAd(@PathVariable("id") Long id, AdRequest adRequest) {
+    return adService.updateAd(adRequest);
+  }
+
+  @PutMapping("/{id}/premium")
+  public AdResponse updateAd(@PathVariable("id") Long id) {
+    return adService.makeAdPremium(id);
+  }
+
+  @DeleteMapping("/{id}")
+  public boolean deleteAd(@PathVariable("id") Long id) {
+    return adService.deleteAd(id);
+  }
+}
