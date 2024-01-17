f8a261bc9378506aa7017ca98321f0b06579fae7
Ridireacht <ridiredire@gmail.com>
2024-01-17 22:46:27 +0300

    Add and integrate Comment repository

--- src/main/java/com/senla/project/services/impl/CommentServiceImpl.java
+++ src/main/java/com/senla/project/services/impl/CommentServiceImpl.java
@@ -1,9 +1,13 @@
 package com.senla.project.services.impl;
 
+import com.senla.project.repositories.CommentRepository;
 import com.senla.project.services.CommentService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CommentServiceImpl implements CommentService {
 
+  private final CommentRepository commentRepository;
 }
