54acb187fdecb1e9a30b6a6a22d1c6632fcb6c3a
Ridireacht <ridiredire@gmail.com>
2024-01-18 05:33:43 +0300

    Style

--- src/main/java/com/senla/project/services/CommentService.java
+++ src/main/java/com/senla/project/services/CommentService.java
@@ -5,7 +5,7 @@ import com.senla.project.dto.CommentResponse;
 import java.util.List;
 
 public interface CommentService {
-  List<CommentResponse> getAllCommentsByAdId(Long id);
+  List<CommentResponse> getAllCommentsByAdId(Long adId);
 
   CommentResponse createComment(Long adId, CommentRequest commentRequest);
 
