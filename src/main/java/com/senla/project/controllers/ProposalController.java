22477ee768c99ca9f559c9b9a776fcd4068e64af
Ridireacht <ridiredire@gmail.com>
2024-01-18 21:16:33 +0300

    Fix method

--- src/main/java/com/senla/project/controllers/ProposalController.java
+++ src/main/java/com/senla/project/controllers/ProposalController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,8 +40,8 @@ public class ProposalController {
     return proposalService.createProposal(proposalRequest);
   }
 
-  @DeleteMapping
-  public boolean declineProposalById(Long id) {
+  @DeleteMapping("{id}")
+  public boolean declineProposalById(@PathVariable("{id}") Long id) {
     return proposalService.declineProposalById(id);
   }
 
