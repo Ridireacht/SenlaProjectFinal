bb6665d17b4818cba8d150bf60ac5493f74e572e
Ridireacht <ridiredire@gmail.com>
2024-01-21 02:28:22 +0300

    Add access and availability checks for Message

--- src/main/java/com/senla/project/controllers/MessageController.java
+++ src/main/java/com/senla/project/controllers/MessageController.java
@@ -2,9 +2,12 @@ package com.senla.project.controllers;
 
 import com.senla.project.dto.ConversationResponse;
 import com.senla.project.dto.MessageRequest;
+import com.senla.project.services.ConversationService;
 import com.senla.project.services.MessageService;
 import com.senla.project.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,11 +20,20 @@ public class MessageController {
 
   private final MessageService messageService;
   private final UserService userService;
+  private final ConversationService conversationService;
 
 
   @PostMapping("/conversations/{id}/messages")
-  public ConversationResponse sendMessage(@PathVariable("id") Long conversationId, MessageRequest messageRequest) {
-    return messageService.sendMessageWithConversationId(getCurrentUserId(), conversationId, messageRequest);
+  public ResponseEntity<ConversationResponse> sendMessage(@PathVariable("id") Long conversationId, MessageRequest messageRequest) {
+    if (conversationService.doesConversationExist(conversationId)) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    if (!conversationService.doesConversationBelongToUser(conversationId, getCurrentUserId())) {
+      return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+
+    return ResponseEntity.ok(messageService.sendMessageWithConversationId(getCurrentUserId(), conversationId, messageRequest));
   }
 
   private Long getCurrentUserId() {
