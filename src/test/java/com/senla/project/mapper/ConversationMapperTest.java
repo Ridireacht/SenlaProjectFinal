d544edd1f71f02cf32f74c80f055f01ebcdf58a1
Ridireacht <ridiredire@gmail.com>
2024-01-22 21:27:25 +0300

    Add ConversationMapper tests

---
+++ src/test/java/com/senla/project/mapper/ConversationMapperTest.java
@@ -0,0 +1,43 @@
+package com.senla.project.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.senla.project.dto.ConversationResponse;
+import com.senla.project.dto.MessageResponse;
+import com.senla.project.entities.Ad;
+import com.senla.project.entities.Conversation;
+import com.senla.project.entities.Message;
+import com.senla.project.entities.User;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConversationMapperTest {
+
+  private final ConversationMapper mapper = Mappers.getMapper(ConversationMapper.class);
+
+  @Test
+  public void testMapToConversationResponse() {
+    Ad ad = new Ad();
+    ad.setId(2L);
+
+    Conversation conversation = new Conversation();
+    conversation.setId(1L);
+    conversation.setAd(ad);
+    conversation.setMessages(new ArrayList<Message>());
+
+    ConversationResponse expectedResponse = new ConversationResponse();
+    expectedResponse.setId(2L);
+    expectedResponse.setAdId(2L);
+    expectedResponse.setMessages(new ArrayList<MessageResponse>());
+
+    ConversationResponse actualResponse = mapper.mapToConversationResponse(conversation);
+
+    assertEquals(expectedResponse.getId(), actualResponse.getId());
+    assertEquals(expectedResponse.getAdId(), actualResponse.getAdId());
+    assertEquals(expectedResponse.getMessages(), actualResponse.getMessages());
+  }
+}
