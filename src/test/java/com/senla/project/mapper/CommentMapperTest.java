757249e5938d7e546a55fe369bb2e143b67471d8
Ridireacht <ridiredire@gmail.com>
2024-01-22 21:52:46 +0300

    Add CommentMapper tests

---
+++ src/test/java/com/senla/project/mapper/CommentMapperTest.java
@@ -0,0 +1,57 @@
+package com.senla.project.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.senla.project.dto.CommentRequest;
+import com.senla.project.dto.CommentResponse;
+import com.senla.project.entities.Comment;
+import com.senla.project.entities.User;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CommentMapperTest {
+
+  private final CommentMapper mapper = Mappers.getMapper(CommentMapper.class);
+
+
+  @Test
+  public void testMapToCommentResponse() {
+    User user = new User();
+    user.setId(2L);
+
+    Comment comment = new Comment();
+    comment.setId(1L);
+    comment.setSender(user);
+    comment.setContent("testContent");
+    comment.setCreatedAt(LocalDateTime.of(2022, 1, 17, 15, 30));
+
+    CommentResponse expectedResponse = new CommentResponse();
+    expectedResponse.setId(1L);
+    expectedResponse.setSenderId(2L);
+    expectedResponse.setContent("testContent");
+    expectedResponse.setCreatedAt(LocalDateTime.of(2022, 1, 17, 15, 30));
+
+    CommentResponse actualResponse = mapper.mapToCommentResponse(comment);
+
+    assertEquals(expectedResponse.getId(), actualResponse.getId());
+    assertEquals(expectedResponse.getSenderId(), actualResponse.getSenderId());
+    assertEquals(expectedResponse.getContent(), actualResponse.getContent());
+    assertEquals(expectedResponse.getCreatedAt(), actualResponse.getCreatedAt());
+  }
+
+  @Test
+  public void testMapToComment() {
+    CommentRequest commentRequest = new CommentRequest();
+    commentRequest.setContent("testContent");
+
+    Comment expectedEntity = new Comment();
+    expectedEntity.setContent("testContent");
+
+    Comment actualEntity = mapper.mapToComment(commentRequest);
+
+    assertEquals(expectedEntity.getContent(), actualEntity.getContent());
+  }
+}
