fde52ff93f6b83b7936149e6af93ba9e2ff03cac
Ridireacht <ridiredire@gmail.com>
2024-01-22 21:21:54 +0300

    Add MessageMapper tests

---
+++ src/test/java/com/senla/project/mapper/MessageMapperTest.java
@@ -0,0 +1,56 @@
+package com.senla.project.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.senla.project.dto.MessageRequest;
+import com.senla.project.dto.MessageResponse;
+import com.senla.project.dto.UserResponse;
+import com.senla.project.dto.UserScoreRequest;
+import com.senla.project.entities.Message;
+import com.senla.project.entities.User;
+import com.senla.project.entities.UserScore;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MessageMapperTest {
+
+  private final MessageMapper mapper = Mappers.getMapper(MessageMapper.class);
+
+  @Test
+  public void testMapToMessageResponse() {
+    User user = new User();
+    user.setId(2L);
+
+    Message message = new Message();
+    message.setSender(user);
+    message.setContent("testContent");
+    message.setPostedAt(LocalDateTime.of(2022, 1, 17, 15, 30));
+
+    MessageResponse expectedResponse = new MessageResponse();
+    expectedResponse.setSenderId(2L);
+    expectedResponse.setContent("testContent");
+    expectedResponse.setPostedAt(LocalDateTime.of(2022, 1, 17, 15, 30));
+
+    MessageResponse actualResponse = mapper.mapToMessageResponse(message);
+
+    assertEquals(expectedResponse.getSenderId(), actualResponse.getSenderId());
+    assertEquals(expectedResponse.getContent(), actualResponse.getContent());
+    assertEquals(expectedResponse.getPostedAt(), actualResponse.getPostedAt());
+  }
+
+  @Test
+  public void testMapToMessage() {
+    MessageRequest messageRequest = new MessageRequest();
+    messageRequest.setContent("testContent");
+
+    Message expectedEntity = new Message();
+    expectedEntity.setContent("testContent");
+
+    Message actualEntity = mapper.mapToMessage(messageRequest);
+
+    assertEquals(expectedEntity.getContent(), actualEntity.getContent());
+  }
+}
