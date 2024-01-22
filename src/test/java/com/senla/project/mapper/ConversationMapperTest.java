e03d3412dec080b050499833a3e822d4ee54f49e
Ridireacht <ridiredire@gmail.com>
2024-01-22 21:45:32 +0300

    Actually test inner objects convertation

--- src/test/java/com/senla/project/mapper/ConversationMapperTest.java
+++ src/test/java/com/senla/project/mapper/ConversationMapperTest.java
@@ -23,20 +23,34 @@ public class ConversationMapperTest {
     Ad ad = new Ad();
     ad.setId(2L);
 
+    ArrayList<Message> messages = new ArrayList<>();
+    messages.add(new Message());
+    messages.add(new Message());
+    messages.get(0).setContent("testContent1");
+    messages.get(1).setContent("testContent2");
+
+    ArrayList<MessageResponse> messageResponses = new ArrayList<>();
+    messageResponses.add(new MessageResponse());
+    messageResponses.add(new MessageResponse());
+    messageResponses.get(0).setContent("testContent1");
+    messageResponses.get(1).setContent("testContent2");
+
     Conversation conversation = new Conversation();
     conversation.setId(1L);
     conversation.setAd(ad);
-    conversation.setMessages(new ArrayList<Message>());
+    conversation.setMessages(messages);
 
     ConversationResponse expectedResponse = new ConversationResponse();
     expectedResponse.setId(1L);
     expectedResponse.setAdId(2L);
-    expectedResponse.setMessages(new ArrayList<MessageResponse>());
+    expectedResponse.setMessages(messageResponses);
 
     ConversationResponse actualResponse = mapper.mapToConversationResponse(conversation);
 
     assertEquals(expectedResponse.getId(), actualResponse.getId());
     assertEquals(expectedResponse.getAdId(), actualResponse.getAdId());
-    assertEquals(expectedResponse.getMessages(), actualResponse.getMessages());
+    assertEquals(expectedResponse.getMessages().size(), actualResponse.getMessages().size());
+    assertEquals(expectedResponse.getMessages().get(0).getContent(), actualResponse.getMessages().get(0).getContent());
+    assertEquals(expectedResponse.getMessages().get(1).getContent(), actualResponse.getMessages().get(1).getContent());
   }
 }
