b7dad672a63c3c9d7f2d7743901da70a48fe9440
Ridireacht <ridiredire@gmail.com>
2024-01-23 23:40:14 +0300

    Make it shorter

--- src/main/java/com/senla/project/mapper/ProposalMapper.java
+++ src/main/java/com/senla/project/mapper/ProposalMapper.java
@@ -3,12 +3,10 @@ package com.senla.project.mapper;
 import com.senla.project.dto.ProposalReceivedResponse;
 import com.senla.project.dto.ProposalRequest;
 import com.senla.project.dto.ProposalSentResponse;
-import com.senla.project.entities.Ad;
 import com.senla.project.entities.Proposal;
 import com.senla.project.repositories.AdRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
@@ -17,6 +15,7 @@ public abstract class ProposalMapper {
   @Autowired
   protected AdRepository adRepository;
 
+
   @Mapping(source = "ad.id", target = "adId")
   abstract ProposalSentResponse mapToProposalSentResponse(Proposal proposal);
 
@@ -24,11 +23,6 @@ public abstract class ProposalMapper {
   @Mapping(source = "sender.id", target = "senderId")
   abstract ProposalReceivedResponse mapToReceivedResponse(Proposal proposal);
 
-  @Mapping(source = "adId", target = "ad", qualifiedByName = "IdToAd")
+  @Mapping(target = "ad", expression = "java(adRepository.findById(proposalRequest.getAdId()).get())")
   abstract Proposal mapToProposal(ProposalRequest proposalRequest);
-
-  @Named("IdToAd")
-  Ad idToAd(Long adId) {
-    return adRepository.findById(adId).orElse(null);
-  }
 }
