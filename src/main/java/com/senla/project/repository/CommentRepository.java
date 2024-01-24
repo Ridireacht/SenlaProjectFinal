cb18bee394f3fa7eec7721e38f3d7cea61dc1385
Ridireacht <ridiredire@gmail.com>
2024-01-25 00:34:00 +0300

    Write a method for JPA-repository

--- src/main/java/com/senla/project/repository/CommentRepository.java
+++ src/main/java/com/senla/project/repository/CommentRepository.java
@@ -1,10 +1,12 @@
 package com.senla.project.repository;
 
 import com.senla.project.entity.Comment;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+  List<Comment> findAllByAd_Id(Long adId);
 }
--- src/main/java/com/senla/project/service/impl/CommentServiceImpl.java
+++ src/main/java/com/senla/project/service/impl/CommentServiceImpl.java
@@ -29,7 +29,7 @@ public class CommentServiceImpl implements CommentService {
 
   @Override
   public List<CommentResponse> getAllCommentsByAdId(Long adId) {
-    List<Comment> comments = commentRepository.findAllByAdId(adId);
+    List<Comment> comments = commentRepository.findAllByAd_Id(adId);
 
     return comments.stream()
         .map(commentMapper::mapToCommentResponse)
