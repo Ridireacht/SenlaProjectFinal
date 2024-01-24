4b02de2023e0f0f28c98f3cf07a22fb8b85ac110
Ridireacht <ridiredire@gmail.com>
2024-01-24 11:43:20 +0300

    Complete UserService

--- src/main/java/com/senla/project/service/impl/UserServiceImpl.java
+++ src/main/java/com/senla/project/service/impl/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.senla.project.service.impl;
 
 import com.senla.project.dto.response.UserResponse;
+import com.senla.project.entity.User;
+import com.senla.project.mapper.AdMapper;
+import com.senla.project.mapper.UserMapper;
 import com.senla.project.repository.UserRepository;
 import com.senla.project.service.UserService;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,26 +16,33 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
   private final UserRepository userRepository;
+  private final UserMapper mapper;
 
 
   @Override
   public List<UserResponse> getAllUsers() {
-    return null;
+    List<User> users = userRepository.findAll();
+
+    return users.stream()
+        .map(mapper::mapToUserResponse)
+        .collect(Collectors.toList());
   }
 
   @Override
   public UserResponse getUserById(Long userId) {
-    return null;
+    User user = userRepository.findById(userId).orElse(null);
+    return mapper.mapToUserResponse(user);
   }
 
   @Override
   public Long getUserIdByUsername(String username) {
-    return null;
+    User user = userRepository.findByUsername(username).orElse(null);
+    return user.getId();
   }
 
   @Override
   public boolean doesUserExist(Long userId) {
-    return false;
+    return userRepository.existsById(userId);
   }
 
 }
