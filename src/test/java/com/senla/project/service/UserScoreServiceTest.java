ef1d6d14aa5dd73462442e716c254dfc6fb5d11c
Ridireacht <ridiredire@gmail.com>
2024-01-25 01:07:27 +0300

    Mock Mapper for UserScoreMapper

--- src/test/java/com/senla/project/service/UserScoreServiceTest.java
+++ src/test/java/com/senla/project/service/UserScoreServiceTest.java
@@ -1,12 +1,14 @@
 package com.senla.project.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 import com.senla.project.dto.request.UserScoreRequest;
 import com.senla.project.dto.response.AdPurchasedResponse;
 import com.senla.project.entity.Ad;
+import com.senla.project.entity.UserScore;
+import com.senla.project.mapper.AdMapper;
+import com.senla.project.mapper.UserScoreMapper;
 import com.senla.project.repository.AdRepository;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
@@ -22,6 +24,12 @@ public class UserScoreServiceTest {
 
   @MockBean
   AdRepository adRepository;
+
+  @MockBean
+  UserScoreMapper userScoreMapper;
+
+  @MockBean
+  AdMapper adMapper;
 
   @Autowired
   UserScoreService userScoreService;
@@ -39,12 +47,22 @@ public class UserScoreServiceTest {
     UserScoreRequest userScoreRequest = new UserScoreRequest();
     userScoreRequest.setScore(score);
 
+    UserScore expectedUserScore = new UserScore();
+    expectedUserScore.setScore(score);
+
+    AdPurchasedResponse expectedAdPurchasedResponse = new AdPurchasedResponse();
+    expectedAdPurchasedResponse.setId(adId);
+    expectedAdPurchasedResponse.setScore(score);
+
     when(adRepository.findById(adId)).thenReturn(Optional.of(ad));
     when(adRepository.save(ad)).thenReturn(ad);
 
-    AdPurchasedResponse result = userScoreService.setUserScoreByAdId(userId, adId, userScoreRequest);
+    when(userScoreMapper.mapToUserScore(userScoreRequest)).thenReturn(expectedUserScore);
+    when(adMapper.mapToAdPurchasedResponse(ad)).thenReturn(expectedAdPurchasedResponse);
 
-    assertEquals(adId, result.getId());
-    assertEquals(score, result.getScore());
+    AdPurchasedResponse actualAdPurchasedResponse = userScoreService.setUserScoreByAdId(userId, adId, userScoreRequest);
+
+    assertEquals(adId, actualAdPurchasedResponse.getId());
+    assertEquals(score, actualAdPurchasedResponse.getScore());
   }
 }
