b09d32db5b00968e8a90fb3486690128315792f8
Ridireacht <ridiredire@gmail.com>
2024-01-24 11:49:02 +0300

    Complete UserScoreService

--- src/main/java/com/senla/project/service/impl/UserScoreServiceImpl.java
+++ src/main/java/com/senla/project/service/impl/UserScoreServiceImpl.java
@@ -2,6 +2,11 @@ package com.senla.project.service.impl;
 
 import com.senla.project.dto.response.AdPurchasedResponse;
 import com.senla.project.dto.request.UserScoreRequest;
+import com.senla.project.entity.Ad;
+import com.senla.project.entity.UserScore;
+import com.senla.project.mapper.AdMapper;
+import com.senla.project.mapper.UserScoreMapper;
+import com.senla.project.repository.AdRepository;
 import com.senla.project.repository.UserScoreRepository;
 import com.senla.project.service.UserScoreService;
 import lombok.AllArgsConstructor;
@@ -12,11 +17,22 @@ import org.springframework.stereotype.Service;
 public class UserScoreServiceImpl implements UserScoreService {
 
   private final UserScoreRepository userScoreRepository;
+  private final AdRepository adRepository;
+
+  private final UserScoreMapper userScoreMapper;
+  private final AdMapper adMapper;
 
 
   @Override
   public AdPurchasedResponse setUserScoreByAdId(Long userId, Long adId,
       UserScoreRequest userScoreRequest) {
-    return null;
+    UserScore userScore = userScoreMapper.mapToUserScore(userScoreRequest);
+
+    Ad ad = adRepository.findById(adId).get();
+    ad.setScore(userScore);
+
+    adRepository.save(ad);
+
+    return adMapper.mapToAdPurchasedResponse(ad);
   }
 }
