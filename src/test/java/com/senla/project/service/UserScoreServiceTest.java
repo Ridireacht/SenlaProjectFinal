b0ce9ff0cee7a6b4399a22078220e19d9000b717
Ridireacht <ridiredire@gmail.com>
2024-01-25 00:09:56 +0300

    Remove unnecessary

--- src/test/java/com/senla/project/service/UserScoreServiceTest.java
+++ src/test/java/com/senla/project/service/UserScoreServiceTest.java
@@ -1,21 +1,13 @@
 package com.senla.project.service;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 import com.senla.project.dto.request.UserScoreRequest;
 import com.senla.project.dto.response.AdPurchasedResponse;
-import com.senla.project.dto.response.UserResponse;
 import com.senla.project.entity.Ad;
-import com.senla.project.entity.User;
 import com.senla.project.repository.AdRepository;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
