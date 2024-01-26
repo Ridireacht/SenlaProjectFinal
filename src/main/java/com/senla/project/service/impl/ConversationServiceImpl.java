15a587e959a33699511e6b9d8a20ab539f424603
Ridireacht <ridiredire@gmail.com>
2024-01-26 13:55:40 +0300

    Complete ConversationServiceImpl

--- src/main/java/com/senla/project/service/impl/ConversationServiceImpl.java
+++ src/main/java/com/senla/project/service/impl/ConversationServiceImpl.java
@@ -1,9 +1,16 @@
 package com.senla.project.service.impl;
 
 import com.senla.project.dto.response.ConversationResponse;
+import com.senla.project.entity.Ad;
+import com.senla.project.entity.Conversation;
+import com.senla.project.entity.User;
+import com.senla.project.mapper.ConversationMapper;
+import com.senla.project.repository.AdRepository;
 import com.senla.project.repository.ConversationRepository;
+import com.senla.project.repository.UserRepository;
 import com.senla.project.service.ConversationService;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,36 +19,62 @@ import org.springframework.stereotype.Service;
 public class ConversationServiceImpl implements ConversationService {
 
   private final ConversationRepository conversationRepository;
+  private final AdRepository adRepository;
+  private final UserRepository userRepository;
+
+  private final ConversationMapper conversationMapper;
 
 
   @Override
   public List<ConversationResponse> getConversationsByUserId(Long userId) {
-    return null;
+    List<Conversation> conversations = conversationRepository.findAllByBuyer_IdOrSeller_Id(userId, userId);
+
+    return conversations.stream()
+        .map(conversationMapper::mapToConversationResponse)
+        .collect(Collectors.toList());
   }
 
   @Override
   public ConversationResponse getConversation(Long conversationId) {
-    return null;
+    Conversation conversation = conversationRepository.findById(conversationId).get();
+    return conversationMapper.mapToConversationResponse(conversation);
   }
 
   @Override
   public ConversationResponse createConversationByAdId(Long userId, Long adId) {
-    return null;
+    Ad ad = adRepository.findById(adId).get();
+    User buyer = userRepository.findById(userId).get();
+
+    Conversation conversation = new Conversation();
+    conversation.setAd(ad);
+    conversation.setBuyer(buyer);
+    conversation.setSeller(ad.getSeller());
+
+    Conversation savedConversation = conversationRepository.save(conversation);
+    return conversationMapper.mapToConversationResponse(savedConversation);
   }
 
   @Override
   public boolean deleteConversation(Long conversationId) {
+    if (conversationRepository.existsById(conversationId)) {
+      conversationRepository.deleteById(conversationId);
+      return true;
+    }
+
     return false;
   }
 
   @Override
   public boolean doesConversationExist(Long conversationId) {
-    return false;
+    return conversationRepository.existsById(conversationId);
   }
 
   @Override
   public boolean doesConversationBelongToUser(Long conversationId, Long currentUserId) {
-    return false;
+    Conversation conversation = conversationRepository.findById(conversationId).get();
+
+    return conversation.getBuyer().getId().equals(currentUserId)
+        || conversation.getSeller().getId().equals(currentUserId);
   }
 
 }
