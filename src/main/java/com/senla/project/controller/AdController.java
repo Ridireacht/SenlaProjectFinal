1b85ef411b1594e2a01b6e356b18fd77996f8cbe
Ridireacht <ridiredire@gmail.com>
2024-01-27 15:36:35 +0300

    Add @ApiResponses

--- src/main/java/com/senla/project/controller/AdController.java
+++ src/main/java/com/senla/project/controller/AdController.java
@@ -8,6 +8,8 @@ import com.senla.project.dto.response.AdResponse;
 import com.senla.project.service.AdService;
 import com.senla.project.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
@@ -36,30 +38,47 @@ public class AdController {
 
 
   @Operation(summary = "Get all ads from others", description = "Получить список всех активных объявлений, которые не принадлежат текущему пользователю")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Успешная операция")
+  })
   @GetMapping
   public List<AdResponse> getAllAdsFromOthers() {
     return adService.getAllAdsFromOthers(getCurrentUserId());
   }
 
   @Operation(summary = "Get current ads", description = "Получить список всех активных объявлений текущего пользователя")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Успешная операция")
+  })
   @GetMapping("/current")
   public List<AdCurrentResponse> getCurrentAds() {
     return adService.getCurrentAdsByUserId(getCurrentUserId());
   }
 
   @Operation(summary = "Get closed ads", description = "Получить список всех неактивных (закрытых) объявлений текущего пользователя")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Успешная операция")
+  })
   @GetMapping("/closed")
   public List<AdClosedResponse> getClosedAds() {
     return adService.getClosedAdsByUserId(getCurrentUserId());
   }
 
   @Operation(summary = "Get purchased ads", description = "Получить список всех выкупленных текущим пользователем объявлений")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Успешная операция")
+  })
   @GetMapping("/purchased")
   public List<AdPurchasedResponse> getPurchasedAds() {
     return adService.getPurchasedAdsByUserId(getCurrentUserId());
   }
 
   @Operation(summary = "Get ad", description = "Получить конкретное объявление по его id")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Успешная операция"),
+      @ApiResponse(responseCode = "404", description = "Объявление не найдено"),
+      @ApiResponse(responseCode = "403", description = "Объявление не доступно текущему пользователю")
+  })
   @GetMapping("/{id}")
   public ResponseEntity<AdResponse> getAd(@PathVariable("id") Long adId) {
     if (!adService.doesAdExist(adId)) {
@@ -74,12 +93,20 @@ public class AdController {
   }
 
   @Operation(summary = "Create ad", description = "Создать новое объявление по форме-реквесту")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Успешная операция")
+  })
   @PostMapping
   public AdResponse createAd(@Valid @RequestBody AdRequest adRequest) {
     return adService.createAd(getCurrentUserId(), adRequest);
   }
 
   @Operation(summary = "Update ad", description = "Обновить существующее объявление по форме-реквесту")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Успешная операция"),
+      @ApiResponse(responseCode = "404", description = "Объявление не найдено"),
+      @ApiResponse(responseCode = "403", description = "Объявление не принадлежит текущему пользователю или уже закрыто")
+  })
   @PutMapping("/{id}")
   public ResponseEntity<AdResponse> updateAd(@PathVariable("id") Long adId, @Valid @RequestBody AdRequest adRequest) {
     if (!adService.doesAdExist(adId)) {
@@ -98,6 +125,11 @@ public class AdController {
   }
 
   @Operation(summary = "Make ad premium", description = "Сделать объявление премиальным по его id")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Успешная операция"),
+      @ApiResponse(responseCode = "404", description = "Объявление не найдено"),
+      @ApiResponse(responseCode = "403", description = "Объявление не принадлежит текущему пользователю или уже закрыто")
+  })
   @PutMapping("/{id}/premium")
   public ResponseEntity<AdResponse> makeAdPremium(@PathVariable("id") Long adId) {
     if (!adService.doesAdExist(adId)) {
@@ -116,6 +148,11 @@ public class AdController {
   }
 
   @Operation(summary = "Delete ad", description = "Полностью удалить объявление по его id")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Успешная операция"),
+      @ApiResponse(responseCode = "404", description = "Объявление не найдено"),
+      @ApiResponse(responseCode = "403", description = "Объявление не принадлежит текущему пользователю или уже закрыто")
+  })
   @DeleteMapping("/{id}")
   public ResponseEntity<Boolean> deleteAd(@PathVariable("id") Long adId) {
     if (!adService.doesAdExist(adId)) {
