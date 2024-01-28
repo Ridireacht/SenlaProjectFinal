d8260c41b77354a69159911f3cdb844d7e9df93a
Ridireacht <ridiredire@gmail.com>
2024-01-29 00:26:29 +0300

    Add UserDetails and UserDetailsService

---
+++ src/main/java/com/senla/project/service/impl/security/UserDetailsServiceImpl.java
@@ -0,0 +1,38 @@
+package com.senla.project.service.impl.security;
+
+import com.senla.project.entity.User;
+import com.senla.project.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import java.util.Optional;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Optional<User> user = userRepository.findByUsername(username);
+
+    return user.map(UserDetailsImpl::new)
+        .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+  }
+
+  @Transactional
+  public String addUser(User user) {
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
+    userRepository.save(user);
+    return "User added successfully. Now log in.";
+  }
+
+}
+
