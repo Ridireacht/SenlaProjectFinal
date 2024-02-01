e5d767025033c697a3b4fdfb6fbe994a64c6ace3
Ridireacht <ridiredire@gmail.com>
2024-02-02 01:12:30 +0300

    Add an additional Ad response for admin

---
+++ src/main/java/com/senla/project/dto/response/AdFullOpenResponse.java
@@ -0,0 +1,19 @@
+package com.senla.project.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdFullOpenResponse {
+  private Long id;
+  private Long sellerId;
+  private String sellerCity;
+  private LocalDateTime postedAt;
+  private String title;
+  private String content;
+  private int price;
+  private boolean isPremium;
+  private double sellerRating;
+}
--- src/main/java/com/senla/project/mapper/AdMapper.java
+++ src/main/java/com/senla/project/mapper/AdMapper.java
@@ -3,27 +3,39 @@ package com.senla.project.mapper;
 import com.senla.project.dto.request.AdRequest;
 import com.senla.project.dto.response.AdClosedResponse;
 import com.senla.project.dto.response.AdCurrentResponse;
+import com.senla.project.dto.response.AdFullOpenResponse;
 import com.senla.project.dto.response.AdPurchasedResponse;
 import com.senla.project.dto.response.AdOpenResponse;
 import com.senla.project.entity.Ad;
+import com.senla.project.repository.RatingRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface AdMapper {
+public abstract class AdMapper {
+
+  @Autowired
+  protected RatingRepository ratingRepository;
+
 
   @Mapping(source = "seller.id", target = "sellerId")
   @Mapping(source = "seller.address", target = "sellerCity")
-  AdOpenResponse mapToAdOpenResponse(Ad ad);
+  public abstract AdOpenResponse mapToAdOpenResponse(Ad ad);
 
-  AdCurrentResponse mapToAdCurrentResponse(Ad ad);
+  @Mapping(source = "seller.id", target = "sellerId")
+  @Mapping(source = "seller.address", target = "sellerCity")
+  @Mapping(target = "sellerRating", expression = "java(ratingRepository.findByUserId(seller.getId()).get().getAverageScore())")
+  public abstract AdFullOpenResponse mapToAdFullOpenResponse(Ad ad);
+
+  public abstract AdCurrentResponse mapToAdCurrentResponse(Ad ad);
 
   @Mapping(source = "buyer.id", target = "buyerId")
   @Mapping(source = "score.score", target = "score")
-  AdClosedResponse mapToAdClosedResponse(Ad ad);
+  public abstract AdClosedResponse mapToAdClosedResponse(Ad ad);
 
   @Mapping(source = "score.score", target = "score")
-  AdPurchasedResponse mapToAdPurchasedResponse(Ad ad);
+  public abstract AdPurchasedResponse mapToAdPurchasedResponse(Ad ad);
 
-  Ad mapToAd(AdRequest adRequest);
+  public abstract Ad mapToAd(AdRequest adRequest);
 }
