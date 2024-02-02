8f79dc85645999845ecee83d65b8a18ce983fe7e
Ridireacht <ridiredire@gmail.com>
2024-02-02 04:34:31 +0300

    Fix JPA

--- src/main/java/com/senla/project/repository/CommentRepository.java
+++ src/main/java/com/senla/project/repository/CommentRepository.java
@@ -4,11 +4,12 @@ import com.senla.project.entity.Comment;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
   @Query("SELECT c FROM Comment c WHERE c.ad.id = :adId")
-  List<Comment> findAllByAdId(long adId);
+  List<Comment> findAllByAdId(@Param("adId") long adId);
 }
